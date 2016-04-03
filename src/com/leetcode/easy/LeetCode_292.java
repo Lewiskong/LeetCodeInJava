10ae2c3b087fb586c9d4c9fea160f91cf3e8b804
孔令玄 <847496788@qq.com>
2016-04-03 17:35:09 +0800

    add problem 292

---
+++ src/com/leetcode/easy/LeetCode_292.java
@@ -0,0 +1,14 @@
+package com.leetcode.easy;
+
+/**
+ * 
+ * 292. Nim Game
+ *	{@linkplain https://leetcode.com/problems/nim-game/}
+ */
+
+public class LeetCode_292 {
+	//omg so stupid
+	public boolean canWinNim(int n) {
+        return n%4==0?false:true;
+    }
+}
