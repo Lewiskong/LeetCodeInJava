91471e13b7f18d8cfc3558be8642c48087ae927c
孔令玄 <847496788@qq.com>
2016-04-13 13:31:23 +0800

    add problem 'reverse linked list'

---
+++ src/com/leetcode/easy/LeetCode_206.java
@@ -0,0 +1,5 @@
+package com.leetcode.easy;
+
+public class LeetCode_206 {
+
+}
--- src/com/leetcode/medium/LeetCode_94.java
+++ src/com/leetcode/medium/LeetCode_94.java
@@ -1,5 +1,30 @@
 package com.leetcode.medium;
 
-public class LeetCode_94 {
+import java.util.ArrayList;
+import java.util.List;
 
+/*
+ * Created by Owen_Kris on 2016年4月13日 12:53:32
+ * Copyright © 2015年 Nirvana. All rights reserved.
+ * 
+ */
+/**
+ * {@linkplain https://leetcode.com/problems/binary-tree-inorder-traversal/}
+ * @author nirvana
+ *
+ */
+// binary tree in order
+public class LeetCode_94 {
+	 public List<Integer> inorderTraversal(TreeNode root) {
+	     List<Integer> result=new ArrayList<Integer>();   
+		 inOrder(root,result);
+		 return result;
+	 }
+	 
+	 private void inOrder(TreeNode root,List<Integer> result){
+		 if (root==null) return;
+		 inOrder(root.left,result);
+		 result.add(root.val);
+		 inOrder(root.right,result);
+	 }
 }
