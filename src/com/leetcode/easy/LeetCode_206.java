6b2a318657fee99b69517fb7c1c92f589d6b48df
孔令玄 <847496788@qq.com>
2016-04-13 20:27:41 +0800

    reverse linked list

--- src/com/leetcode/easy/LeetCode_206.java
+++ src/com/leetcode/easy/LeetCode_206.java
@@ -1,5 +1,24 @@
 package com.leetcode.easy;
-
+/*
+ * Created by Owen_Kris on 2016年4月13日 12:59:20
+ * Copyright © 2015年 Nirvana. All rights reserved.
+ * 
+ */
+/**
+ * {@linkplain https://leetcode.com/problems/reverse-linked-list/}
+ * @author nirvana
+ * 
+ */
+// Reverse Linked List
 public class LeetCode_206 {
-
+	public ListNode reverseList(ListNode head) {
+		if (head==null) return null;
+		ListNode result=new ListNode(head.val);
+		for (ListNode temp=head.next;temp!=null;temp=temp.next){
+			ListNode t=result;
+			result=new ListNode(temp.val);
+			result.next=t;
+		}
+		return result;
+    }
 }
