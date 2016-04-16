67593602235a5fc87dc4dcdb330e30c59070e256
孔令玄 <847496788@qq.com>
2016-04-17 00:44:12 +0800

    add problem romman to integer

--- src/com/leetcode/easy/LeetCode_13.java
+++ src/com/leetcode/easy/LeetCode_13.java
@@ -13,8 +13,64 @@ package com.leetcode.easy;
 // roman to integer
 // I（1）、V（5）、X（10）、L（50）、C（100）、D（500）和M（1000）
 public class LeetCode_13 {
-	
 	public int romanToInt(String s) {
-		return 0;
+		int result=0;
+		int left=0;
+		for (int index=0;index<s.length();){
+			int pos=checkMax(s,index);
+			result+=getResult(s,pos,left);
+			index=pos+1;
+			left=index;
+		}
+		return result;
 	}
+	
+	private int checkMax(String str,int index){
+		String temp=str.substring(index);
+		int result;
+		if ((result=temp.indexOf('M'))!=-1) return result+index;
+		else if((result=temp.indexOf('D'))!=-1) return result+index;
+		else if ((result=temp.indexOf('C'))!=-1) return result+index;
+		else if ((result=temp.indexOf('L'))!=-1) return result+index;
+		else if ((result=temp.indexOf('X'))!=-1) return result+index;
+		else if ((result=temp.indexOf('V'))!=-1) return result+index;
+		else if ((result=temp.indexOf('I'))!=-1) return result+index;
+		return -1;
+	}
+	
+	private int revertState(char c){
+		switch(c){
+		case('M'):
+			return 1000;
+		case('D'):
+			return 500;
+		case('C'):
+			return 100;
+		case('L'):
+			return 50;
+		case('X'):
+			return 10;
+		case('V'):
+			return 5;
+		case('I'):
+			return 1;
+		default:
+			throw new RuntimeException("wrong revert number"+c);
+		}
+	}
+	
+	private int getResult(String str,int rightIndex,int leftIndex){
+		int result=revertState(str.charAt(rightIndex));
+		for (int index=rightIndex-1;index>=leftIndex;index--){
+			result-=revertState(str.charAt(index));
+		}
+		return result;
+	}
+	
+	public static void main(String[] args){
+		LeetCode_13 l=new LeetCode_13();
+		int result=l.romanToInt("IX");
+		System.out.println(result);
+	}
+	
 }
