de008821af8ad56101bb62946d498af32b7165aa
孔令玄 <847496788@qq.com>
2016-04-16 23:43:45 +0800

    add romman to integer

---
+++ src/com/leetcode/easy/LeetCode_13.java
@@ -0,0 +1,20 @@
+package com.leetcode.easy;
+
+/*
+ * Created by Owen_Kris on 2016年4月15日 09:34:15
+ * Copyright © 2015年 Nirvana. All rights reserved.
+ * 
+ */
+/**
+ * {@linkplain https://leetcode.com/problems/roman-to-integer/}
+ * @author nirvana
+ *
+ */
+// roman to integer
+// I（1）、V（5）、X（10）、L（50）、C（100）、D（500）和M（1000）
+public class LeetCode_13 {
+	
+	public int romanToInt(String s) {
+		return 0;
+	}
+}
